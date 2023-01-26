45828adf131c7ece93f70036ebe83f6cb278901e
Mariska Magyar <magyarmariska@gmail.com>
2023-01-26 21:01:15 +0100

    feat(index): test

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
   const [textValue, setTextValue] = useState("");
 
   const handleAdd = () => {
-    console.log('hello world! 10')
+    console.log('hello world! 11')
     if (!textValue || items.length >= 8) {
       return;
     }
