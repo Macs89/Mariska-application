5c43880621901f89c6a3256573857ec60008de2d
Mariska Magyar <magyarmariska@gmail.com>
2023-04-27 13:54:34 +0200

    find node version

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
   const [textValue, setTextValue] = useState("");
 
   const handleAdd = () => {
-    console.log('hello world! 24')
+    console.log('hello world! 24', process?.version ?? `not found`)
     if (!textValue || items.length >= 8) {
       return;
     }
